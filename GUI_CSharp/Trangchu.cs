5ffe5b1d65b861e26e50fa62bb2c115e2df3b379
LTHuu <117341204+LTHuu@users.noreply.github.com>
2024-11-22 17:43:30 +0700

    hoàn thành merge

--- GUI_CSharp/Trangchu.cs
+++ GUI_CSharp/Trangchu.cs
@@ -547,7 +547,6 @@ namespace GUI_CSharp
             {
                 cardListGV.Visible = true;
                 cardListPhancong.Visible = false;
-                LoadGiaoVienList();
             }
 
 
